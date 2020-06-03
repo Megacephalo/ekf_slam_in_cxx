15d4f35c148128df984aceeb4bc3f7c3e62174cc
Charly Huang <charly.charlongo@gmail.com>
2020-06-03 05:30:18 -0400

    Add a Python script to ease in inputing execution commads

---
+++ run_main.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+#! /usr/bin/python3
+
+import os
+import sys
+
+if __name__=='__main__':
+    print('Running EKF SLAM')
+
+    dataset_dir = 'data/'
+    sensor_dataset = dataset_dir + 'sensor_data.dat'
+    world_dataset = dataset_dir + 'world.dat'
+
+    if not sensor_dataset:
+    	print('{} does not exist and cannot get any information on sensor observations.')
+    	sys.exit()
+
+    if not world_dataset:
+    	print('{} does not exist and cannot get any information on the mapping information.')
+    	sys.exit()
+
+    # If all files exist, let's go through the normal procedure
+    bin_dir = 'bin/'
+    exe_name = bin_dir + 'EKF_SLAM_in_cxx'
+    os.system(exe_name + ' ' + sensor_dataset + ' ' + world_dataset)
+    print('Done! ')
